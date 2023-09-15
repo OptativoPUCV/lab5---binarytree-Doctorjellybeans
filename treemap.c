a5722684d5db14b00f84d6791d27fc764a2a1c74
Replit user <>
2023-09-15 20:02:50 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -311,9 +311,9 @@ Pair * nextTreeMap(TreeMap * tree) {
     return NULL;
   }
   TreeNode* nodoCurrent = tree->root;
-  TreeNode* nodoCurrentKey = tree->root->pair->key;
+  TreeNode* nodoCurrentKey = tree->root->pair->value;
   TreeNode* sucesor = NULL;
-  TreeNode* treeCurrentKey = tree->current->pair->key;
+  TreeNode* treeCurrentKey = tree->current->pair->value;
 
   while (nodoCurrent != NULL){
     printPair(nodoCurrent->pair);
